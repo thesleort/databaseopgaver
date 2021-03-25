8576a6fd74fb984b0521901c541cee786e421979
Troels Blicher Petersen <troels@newtec.dk>
2021-03-25 16:10:16 +0100

    Fixes to aflevering

--- aflevering 1/aflevering.sql
+++ aflevering 1/aflevering.sql
@@ -4,22 +4,50 @@ JOIN bil ON kunde.kunde_id = bil.kunde_id
 JOIN reparation ON reparation.bil_id = bil.bil_id 
 JOIN mekaniker ON mekaniker.mekaniker_id = reparation.mekaniker_id 
 WHERE mekaniker.navn = "Palle";
++-------------------+
+| navn              |
++-------------------+
+| Henrik Hemmingsen |
+| Asger Johansen    |
++-------------------+
+
 
 -- Opgave 2
 SELECT navn FROM reservedel 
 LEFT OUTER JOIN resrep ON resrep.reservedel_id = reservedel.reservedel_id 
 WHERE resrep.reservedel_id IS NULL;
++---------+
+| navn    |
++---------+
+| Bremser |
+| Tandrem |
++---------+
 
 -- Opgave 3
 SELECT navn FROM kunde 
 JOIN bil ON kunde.kunde_id = bil.kunde_id 
 GROUP BY navn 
 HAVING COUNT(*) > 1;
++----------------+
+| navn           |
++----------------+
+| Asger Johansen |
+| Rasmus Iversen |
++----------------+
+
 
 -- Opgave 4
 SELECT registreringsnr,model FROM bil 
 JOIN reparation ON reparation.bil_id = bil.bil_id 
 WHERE YEAR(dato) = 2019;
++-----------------+-------------+
+| registreringsnr | model       |
++-----------------+-------------+
+| MA45647         | Renault 4CV |
+| MH40136         | Opel Kadett |
+| XP37918         | VW Up!      |
++-----------------+-------------+
+
 
 -- Opgave 5
 UPDATE reservedel 
@@ -52,20 +80,20 @@ FROM reparation
 JOIN mekaniker ON reparation.mekaniker_id = mekaniker.mekaniker_id 
 JOIN bil ON bil.bil_id = reparation.bil_id 
 WHERE aargang < 2021 - 25 
-GROUP BY navn;
-+-------+----------+
-| navn  | COUNT(*) |
-+-------+----------+
-| Poul  |        2 |
-| Palle |        1 |
-+-------+----------+
+GROUP BY navn 
+ORDER BY navn DESC LIMIT 1;
++------+
+| navn |
++------+
+| Poul |
++------+
 
 -- Opgave 8
 -- Nogle reparationer koster ikke noget i form af nye dele.
 -- Priserne er efter prisstigningen på 7%
 SELECT model,AVG(pris) FROM (
   SELECT model,IFNULL(pris,0) as pris,resrep_id FROM bil 
-  JOIN reparation ON bil.bil_id = reparation.bil_id 
+  LEFT OUTER JOIN reparation ON bil.bil_id = reparation.bil_id 
   LEFT OUTER JOIN resrep ON resrep.reparation_id = reparation.reparation_id 
   LEFT OUTER JOIN reservedel ON reservedel.reservedel_id = resrep.reservedel_id 
   GROUP BY model,pris,resrep_id) 
@@ -73,6 +101,7 @@ sub GROUP BY 1;
 +------------------+-----------+
 | model            | AVG(pris) |
 +------------------+-----------+
+| Renault Clio     |    0.0000 |
 | Opel Kadett      |  695.5000 |
 | Renault 4CV      |  535.0000 |
 | VW Up!           |  321.0000 |
@@ -85,7 +114,7 @@ JOIN reparation ON mekaniker.mekaniker_id = reparation.mekaniker_id
 JOIN resrep ON resrep.reparation_id = reparation.reparation_id 
 JOIN reservedel ON reservedel.reservedel_id = resrep.reservedel_id 
 GROUP BY navn 
-ORDER BY indtjening DESC;
+ORDER BY indtjening DESC LIMIT 1;
 +-------+------------+
 | navn  | indtjening |
 +-------+------------+
@@ -99,12 +128,11 @@ SELECT mekaniker.navn,SUM(PRIS) as "Samlet indtjent",COUNT(dato) as "Antal arbej
 JOIN reparation ON mekaniker.mekaniker_id = reparation.mekaniker_id 
 JOIN resrep ON resrep.reparation_id = reparation.reparation_id 
 JOIN reservedel ON resrep.reservedel_id = reservedel.reservedel_id 
-WHERE QUARTER(dato) = 1 OR QUARTER(dato) = 4 
+WHERE (QUARTER(dato) = 1 OR QUARTER(dato) = 4) AND mekaniker.navn != "Poul"
 GROUP BY navn;
 +-------+-----------------+---------------+
 | navn  | Samlet indtjent | Antal arbejde |
 +-------+-----------------+---------------+
-| Poul  |             963 |             2 |
 | Palle |             963 |             2 |
 | Per   |            1391 |             1 |
 +-------+-----------------+---------------+
